b70ecf1eca082098df94a988964eede7fc800484
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-10-13 21:03:50 -0700

    new storage files

---
+++ models/base_model.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ('created_at', 'updated_at'):
+                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
+            self.created_at = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            self.updated_at = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            storage.new(self)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            storage.new(self)
+
+    def save(self):
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+
+    def __str__(self):
+        class_name = self.__class__.__name__
+        return f"[{class_name}] ({self.id}) {self.__dict__}"
---
+++ models/engine/file_storage.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python3
+import cmd
+from models import storage
+from models.base_model import BaseModel
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program using EOF (Ctrl+D)"""
+        return True
+
+    def emptyline(self):
+        pass
+
+    def do_create(self, arg):
+        """Create a new instance of BaseModel and save it"""
+        if not arg:
+            print("** class name missing **")
+        else:
+            class_name = arg
+            if class_name not in BaseModel.__subclasses__():
+                print("** class doesn't exist **")
+            else:
+                new_instance = BaseModel()
+                new_instance.save()
+                print(new_instance.id)
+
+    def do_show(self, arg):
+        """Print the string representation of an instance"""
+        if not arg:
+            print("** class name missing **")
+            return
+        args = arg.split()
+        if args[0] not in BaseModel.__subclasses__():
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            objects = storage.all()
+            key = args[0] + '.' + args[1]
+            if key in objects:
+                print(objects[key])
+            else:
+                print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """Delete an instance"""
+        if not arg:
+            print("** class name missing **")
+            return
+        args = arg.split()
+        if args[0] not in BaseModel.__subclasses__():
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            objects = storage.all()
+            key = args[0] + '.' + args[1]
+            if key in objects:
+                del objects[key]
+                storage.save()
+            else:
+                print("** no instance found **")
+
+    def do_all(self, arg):
+        """Print all instances or all instances of a specific class"""
+        objects = storage.all()
+        if not arg:
+            print([str(obj) for obj in objects.values()])
+        elif arg not in BaseModel.__subclasses__():
+            print("** class doesn't exist **")
+        else:
+            print([str(obj) for key, obj in objects.items() if key.split('.')[0] == arg])
+
+    def do_update(self, arg):
+        """Update an instance's attribute"""
+        if not arg:
+            print("** class name missing **")
+            return
+        args = arg.split()
+        if args[0] not in BaseModel.__subclasses__():
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        objects = storage.all()
+        key = args[0] + '.' + args[1]
+        if key not in objects:
+            print("** no instance found **")
+            return
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+        if len(args) < 4:
+            print("** value missing **")
+            return
+        instance = objects[key]
+        attr_name = args[2]
+        attr_value = args[3]
+        try:
+            attr_value = eval(attr_value)
+        except:
+            pass
+        setattr(instance, attr_name, attr_value)
+        instance.save()
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
