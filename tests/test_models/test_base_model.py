db483e6697eb32438aaa9d281e2cbe7e6d491995
phillip mugisawq <phillipmugisa@icloud.com>
2023-10-17 07:20:15 +0300

    base model tests written

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -13,8 +13,46 @@ class TestBaseModel(unittest.TestCase):
         model = BaseModel()
         self.assertTrue(model.id)
 
+    def test_id_is_string(self):
+        model = BaseModel()
+        self.assertIsInstance(model.id, str)
+
+    def test_created_at_assign(self):
+        m = BaseModel()
+        self.assertTrue(m.created_at)
+
+    def test_updated_at_assign(self):
+        m = BaseModel()
+        self.assertTrue(m.created_at)
+
+    def assert_updated_at_equal_created_at(self):
+        m = BaseModel()
+        self.assertEqual(m.created_at, m.updated_at)
+
+    def test_updated_at_changes(self):
+        m = BaseModel()
+        # store current updated at
+        c_updated_at = m.updated_at
+        m.save()
+        self.assertNotEqual(m.updated_at, c_updated_at)
+
+    def test_save(self):
+        m = BaseModel()
+        # store current updated at
+        c_updated_at = m.updated_at
+        m.save()
+        self.assertNotEqual(m.updated_at, c_updated_at)
+
+    def test_to_dict(self):
+        m = BaseModel()
+        m_dict = m.to_dict()
+        self.assertEqual(m_dict['__class__'], m.__class__.__name__)
+        self.assertEqual(m_dict['created_at'], m.created_at.isoformat())
+        self.assertEqual(m_dict['updated_at'], m.updated_at.isoformat())
+
     def test_str(self):
-        pass
+        m = BaseModel()
+        self.assertEqual(m.__str__(), f"[{m.__class__.__name__}] ({m.id}) {m.__dict__}")
 
 
 if __name__ == "__main__":
