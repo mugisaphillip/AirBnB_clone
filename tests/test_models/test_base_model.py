408f4c549fe5070fa350bd6c015e9090763a360f
phillip mugisawq <phillipmugisa@icloud.com>
2023-10-17 08:09:42 +0300

    pycodestyle passing

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,6 +7,7 @@ import unittest
 from datetime import datetime
 from models.base_model import BaseModel
 
+
 class TestBaseModel(unittest.TestCase):
     """Unittests for testing of the BaseModel class."""
 
@@ -53,7 +54,8 @@ class TestBaseModel(unittest.TestCase):
 
     def test_str(self):
         m = BaseModel()
-        self.assertEqual(m.__str__(), f"[{m.__class__.__name__}] ({m.id}) {m.__dict__}")
+        test_data = f"[{m.__class__.__name__}] ({m.id}) {m.__dict__}"
+        self.assertEqual(m.__str__(), test_data)
 
 
 class TestBaseModel_from_dict(unittest.TestCase):
@@ -63,19 +65,25 @@ class TestBaseModel_from_dict(unittest.TestCase):
 
     def test_arguments_passed(self):
         test_data = {'id': 'a592f7d4-ba37-44b8-a381-4b9f51639895',
-                    'created_at': '2023-10-17T07:29:05.227683',
-                    'updated_at': '2023-10-17T07:29:05.227683',
-                    'name': 'My_First_Model', 'my_number': 89,
-                    '__class__': 'BaseModel'}
+                     'created_at': '2023-10-17T07:29:05.227683',
+                     'updated_at': '2023-10-17T07:29:05.227683',
+                     'name': 'My_First_Model', 'my_number': 89,
+                     '__class__': 'BaseModel'}
 
         b = BaseModel(**test_data)
         self.assertEqual(b.id, test_data["id"])
-        self.assertEqual(b.created_at, datetime.strptime(test_data['created_at'],
-                                                '%Y-%m-%dT%H:%M:%S.%f'))
-        self.assertEqual(b.updated_at, datetime.strptime(test_data['updated_at'],
-                                                '%Y-%m-%dT%H:%M:%S.%f'))
+
+        created_at = datetime.strptime(test_data['created_at'],
+                                       '%Y-%m-%dT%H:%M:%S.%f')
+        self.assertEqual(b.created_at, created_at)
+
+        updated_at = datetime.strptime(test_data['updated_at'],
+                                       '%Y-%m-%dT%H:%M:%S.%f')
+        self.assertEqual(b.updated_at, updated_at)
+
         self.assertEqual(b.name, "My_First_Model")
         self.assertEqual(str(b.my_number), "89")
+
 
 if __name__ == "__main__":
     unittest.main()
